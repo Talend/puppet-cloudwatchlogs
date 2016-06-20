7b3c41b90d2f878ab9a8f3e99a14bdd57d626b15
aheumaier <andreas.heumaier@nordcloud.com>
2016-06-21 00:39:59 +0200

    refactor beaker spec tests

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,32 +1,12 @@
 require 'spec_helper_acceptance'
 
+describe 'cloudwatchlogs' do
 
-describe "cloudwatchlogs" do
-  let(:pp) do
-    <<-EOS
-    cloudwatchlogs::log{'Beakertest'
+  it_should_behave_like 'cloudwatchlogs::installed', "
       path            => '/var/log/messages',
       streamname      => 'thisIsABeakerTestHost'
-    }
-    EOS
-  end
+  "
 
-  it_behaves_like 'puppet::appliable', :pp
-
-  context 'should have cloudwatchlogs role configured' do
-
-    describe file('/var/log/awslogs.log'), :retry => 3, :retry_wait => 10 do
-
-      # TODO: missing wait condition makes this fail
-      # its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/audit,\ log stream/ }
-      # its(:content) { should match /Log group:\ \/talend\/tic\/base\/var\/log\/messages,\ log stream/ }
-      its(:content) { should match /Log group:\ thisIsABeakerTestHost\/Beakertest,\ log stream/ }
-
-      its(:content) { should_not match /ERROR:/ }
-    end
-
-  end
-
+  it_should_behave_like 'cloudwatchlogs::running'
 
 end
-
