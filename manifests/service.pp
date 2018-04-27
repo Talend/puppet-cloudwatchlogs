9a4612f48349805d9fcfa6382ee7cda7484ae3a2
Erwan SEITE <eseite@talend.com>
2018-04-27 15:45:42 +0200

    * add script for local tests
    * fix fixture to be equal to Puppetfile.lock
    
    * improve venv management

--- manifests/service.pp
+++ manifests/service.pp
@@ -14,16 +14,31 @@ class cloudwatchlogs::service (
   exec { 'cloudwatchlogs-install':
     path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
     command => "python /usr/local/src/awslogs-agent-setup.py -n -r ${region} -c /etc/awslogs/awslogs.conf",
-    onlyif  => '[ -e /usr/local/src/awslogs-agent-setup.py ]',
-    unless  => '[ -d /var/awslogs/bin ]',
+    onlyif  => '/bin/test -f /usr/local/src/awslogs-agent-setup.py',
+    unless  => '/bin/test -d /var/awslogs/bin',
     require => Concat['/etc/awslogs/awslogs.conf'],
   }
 
-  exec { 'cloudwatchlogs-update-config':
-    command => "python /usr/local/src/awslogs-agent-setup.py -n -r ${region} -c /etc/awslogs/awslogs.conf",
-    path    => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
-    onlyif  => 'test /etc/awslogs/awslogs.conf -nt /var/awslogs/etc/awslogs.conf',
+  exec { 'pyenv-update-pip':
+    command => '/var/awslogs/bin/pip install -U pip && /bin/touch /var/awslogs/.pip_updated',
+    unless  => '/bin/test -f /var/awslogs/.pip_updated',
     require => Exec['cloudwatchlogs-install']
+  }
+
+  exec { 'pyenv-update-setuptools':
+    command => '/var/awslogs/bin/pip install -U setuptools && /bin/touch /var/awslogs/.setuptools_updated',
+    unless  => '/bin/test -f /var/awslogs/.setuptools_updated',
+    require => Exec['pyenv-update-pip']
+  }
+
+  exec { 'cloudwatchlogs-update-config':
+    command => "/var/awslogs/bin/python /usr/local/src/awslogs-agent-setup.py -n -r ${region} -c /etc/awslogs/awslogs.conf",
+    onlyif  => '/bin/test /etc/awslogs/awslogs.conf -nt /var/awslogs/etc/awslogs.conf',
+    require => [
+      Exec['cloudwatchlogs-install'],
+      Exec['pyenv-update-pip'],
+      Exec['pyenv-update-setuptools']
+    ]
   } ~>
 
   service { 'awslogs':
